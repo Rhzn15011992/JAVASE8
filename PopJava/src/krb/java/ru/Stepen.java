13ec199847dd8bb75a2dd987a0c142eef3f32011
HP <HP@LAPTOP-04N905MF>
2017-01-29 00:47:30 +0300

    zd3

--- PopJava/src/krb/java/ru/Stepen.java
+++ PopJava/src/krb/java/ru/Stepen.java
@@ -8,16 +8,16 @@ public class Stepen {
 
 	public static void main(String[] args) throws IOException {
 		/** возведение в степень **/
-		System.out.println(stepen(2, 3));
-
+	
+		asd(2,3);
 	}
 
-	public static int stepen(int a, int b) {
-
-		for (int i = 1; i < b; i++) {
-			a = a * 2;
-		}
-
-		return a;
+	public static void asd(int a, int b){
+		
+	System.out.println(Math.pow(a, b));
 	}
+
+
+
+
 }
