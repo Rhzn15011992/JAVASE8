c8917ac7598a8420e2c57a5bce3bace8582a6713
HP <HP@LAPTOP-04N905MF>
2017-01-28 13:59:37 +0300

    proverka

---
+++ PopJava/src/krb/java/ru/MyFirstClass.java
@@ -0,0 +1,16 @@
+package krb.java.ru;
+
+public class MyFirstClass {
+	
+	
+	
+	public static void main(String[]args)
+	{
+		int matrix[][] = new int [2][2];
+		int [] tmp;
+		tmp = matrix[1];
+		matrix[0][0] - 
+		
+	}
+
+}
