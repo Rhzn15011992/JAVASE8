24a1d481e4874a0b8955af20875be3191d54c47a
HP <HP@LAPTOP-04N905MF>
2017-01-28 21:55:40 +0300

    DZ

---
+++ PopJava/src/krb/java/ru/Stepen.java
@@ -0,0 +1,26 @@
+package krb.java.ru;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Stepen {
+
+	public static void main(String[] args)throws IOException {
+		System.out.println(stepen(2,3));
+     
+	}
+
+	public static int stepen(int a, int b)
+	{
+		
+		
+		for(int i = 1; i < b; i ++)
+		{
+		
+			a = a * 2;
+		}
+		
+		return a;
+	}
+}
