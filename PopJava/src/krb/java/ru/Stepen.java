5df026b5d3eef6c82f314e61b68e68c69ef27de8
HP <HP@LAPTOP-04N905MF>
2017-01-31 19:46:49 +0300

    sada

--- PopJava/src/krb/java/ru/Stepen.java
+++ PopJava/src/krb/java/ru/Stepen.java
@@ -6,15 +6,42 @@ import java.io.InputStreamReader;
 
 public class Stepen {
 
-	public static void main(String[] args) throws IOException {
-		/** возведение в степень **/
-	
-		asd(2,3);
-	}
+	public static void asd() throws IOException 
+	{ 
+	/** Возведение числа в степень **/ 
+	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
 
-	public static void asd(int a, int b){
-		
-	System.out.println(Math.pow(a, b));
+	String [] list = new String[3]; 
+	list[0] = "Число - "; 
+	list[1] = " в степени - "; 
+	list[2] = " = "; 
+
+	System.out.println("Введите число которое будем возводить в степень и нажмите Enter"); 
+	double a = Double.parseDouble(reader.readLine()); 
+	System.out.println("Введите степень в которую будем возводить и нажмите Enter"); 
+	int b = Integer.parseInt(reader.readLine()); 
+	double result = 1; 
+	for (int i = 0; i < b; result = result * a) 
+	{ 
+	if (b == 0) 
+	{ 
+	result = 1; 
+	break; 
+	} 
+	if (a == 0) 
+	{ 
+	result = 0; 
+	} 
+	// 
+	if (b % 2 == 0 && a < 0) 
+	{ 
+	result = result * -1; 
+	} 
+	i++; 
+	} 
+
+	System.out.println(list[0]+a+list[1]+b+list[2]+(int)result); 
+
 	}
 
 
